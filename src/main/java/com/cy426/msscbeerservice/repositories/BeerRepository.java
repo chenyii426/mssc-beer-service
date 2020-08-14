59656154611591a20bff352076336e7188f76962
chenyii426 <chenyii426@gmail.com>
2020-08-14 01:30:05 -0700

    Add beer repository. Close #6

---
+++ src/main/java/com/cy426/msscbeerservice/repositories/BeerRepository.java
@@ -0,0 +1,14 @@
+package com.cy426.msscbeerservice.repositories;
+
+import com.cy426.msscbeerservice.domain.Beer;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface BeerRepository extends PagingAndSortingRepository<Beer,UUID>{
+
+}
