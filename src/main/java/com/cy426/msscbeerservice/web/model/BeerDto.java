890160411ee481386afa87a223f3823b1ac3349d
chenyii426 <chenyii426@gmail.com>
2020-08-14 12:45:32 -0700

    Adding Bean Validation. Close #7

--- src/main/java/com/cy426/msscbeerservice/web/model/BeerDto.java
+++ src/main/java/com/cy426/msscbeerservice/web/model/BeerDto.java
@@ -9,24 +9,41 @@ import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 import java.util.UUID;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Null;
+import javax.validation.constraints.Positive;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class BeerDto {
 
+    @Null
     private UUID id;
+
+    @Null
     private Integer version;
 
+    @Null
     private OffsetDateTime createDate;
+
+    @Null
     private OffsetDateTime lastModifiedDate;
 
+    @NotBlank
     private String beerName;
 
+    @NotNull
     private BeerStyleEnum beerStyle;
 
+    @Positive
+    @NotNull
     private Long upc;
 
+    @Positive
+    @NotNull
     private BigDecimal price;
 
     private Integer quantityOnHand;
